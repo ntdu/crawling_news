385cc7e07dcd5f3bfa2f2f00ed1c24dfb43dd2dd
danielsolid <danielsolid@solidbytes.vn>
2022-10-25 16:46:37 +0700

    update middleware

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
